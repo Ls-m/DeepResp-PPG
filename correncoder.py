311f5b6b164acb677111060700e17f24e303941c
ls-m <elham.fr80@gmail.com>
2025-04-25 19:47:57 +0330

    init another model

---
+++ correncoder.py
@@ -0,0 +1,85 @@
+from tqdm import tqdm
+import pandas as pd
+import os
+import torch
+import numpy as np
+import random
+
+
+# a function that gives a rough indication of breaths per minute error by examining the crossings of 0.5
+# this assumes that the respiratory reference is normalised between 0 and 1.
+def breaths_per_min_zc(output_array_zc, input_array_zc):
+    peak_count_output = []
+    peak_count_cap = []
+    for ind_output in range(output_array_zc.shape[0]):
+        output_array_zc_temp = output_array_zc[ind_output, 0, :]
+        input_array_zc_temp = input_array_zc[ind_output, :]
+        output_array_zc_temp = output_array_zc_temp - 0.5
+        input_array_zc_temp = input_array_zc_temp - 0.5
+        zero_crossings_output = ((output_array_zc_temp[:-1] * output_array_zc_temp[1:]) < 0).sum()
+        zero_crossings_input = ((input_array_zc_temp[:-1] * input_array_zc_temp[1:]) < 0).sum()
+        peak_count_output.append(zero_crossings_output)
+        peak_count_cap.append(zero_crossings_input)
+        # breaths_per_min_output = (zero_crossings_output / 2)*6.25
+    peak_count_output = np.array(peak_count_output)
+    peak_count_cap = np.array(peak_count_cap)
+    #6.5 is used ot scale up to 1 minute, as each segment here is 60/6.5 seconds long.
+    mean_error = ((np.mean(peak_count_output - peak_count_cap)) / 2) * 6.5
+    mean_abs_error = ((np.mean(np.abs(peak_count_output - peak_count_cap))) / 2) * 6.5
+    return mean_abs_error, mean_error
+
+
+ppg_csv_path = "/Users/eli/Downloads/PPG Data/csv"
+ppg_csv_files = [f for f in os.listdir(ppg_csv_path) if f.endswith('.csv') and not f.startswith('.DS_Store')]
+input_name = 'PPG'
+target_name = 'NASAL CANULA'
+
+num_of_subjects = 0
+
+
+
+# define number of epochs and batch size
+num_epochs = 80
+batch_size = 30
+
+#define number of kernels per layer
+n_in, n_out = 1, 8
+n_out2 = 8
+n_out3 = 8
+n_outputs = 1
+
+# define kernel lengths, padding, dilation, stride, and dropout
+kernel_size = 150
+kernel_size2 = 75
+kernel_size3 = 50
+padding = 20
+dilation = 1
+stride = 1
+dropout_val = 0.5
+padding2 = 20
+padding3 = 10
+dilation2 = 1
+dilation3 = 1
+stride2 = 1
+stride3 = 1
+
+# set a seed for evaluation (optional)
+seed_val = 55
+print("Seed")
+print(seed_val)
+torch.manual_seed(seed_val)
+random.seed(seed_val)
+np.random.seed(seed_val)
+
+# set the learning rate for Adam optimisation
+learning_rate = 0.001
+
+
+kf = KFold(42)
+kf.get_n_splits(data_ppg)
+sub_num = 1
+
+for ppg_file in tqdm(ppg_csv_files, leave=True) :
+    data = pd.read_csv(os.path.join(ppg_csv_path, ppg_file), sep='\t', index_col='Time', skiprows=[1])
+    if input_name in data.columns and target_name in data.columns:
+        num_of_subjects += 1
