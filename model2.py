9d9ac44eed68dd1ad9b37a2d7be2b8e022992263
Elham Farang <elham.fr80@gmail.com>
2025-04-28 17:39:17 +0330

    model correncoder added

---
+++ model2.py
@@ -0,0 +1,42 @@
+import torch
+
+import torch.nn as nn
+
+class Correncoder_model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv1d(n_in, n_out, kernel_size=kernel_size, padding=padding),
+            nn.ReLU(),
+            nn.Dropout(dropout_val)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv1d(n_out, n_out2, kernel_size=kernel_size2, padding=padding2),
+            nn.ReLU(),
+            nn.Dropout(dropout_val)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv1d(n_out2, n_out3, kernel_size=kernel_size3, padding=padding3),
+            nn.Sigmoid(),
+            nn.Dropout(dropout_val)
+        )
+        self.layer4 = nn.Sequential(
+            nn.ConvTranspose1d(n_out3, n_out2, kernel_size=kernel_size3, padding=padding3),
+            nn.Sigmoid()
+        )
+        self.layer5 = nn.Sequential(
+            nn.ConvTranspose1d(n_out2, n_out, kernel_size=kernel_size2, padding=padding2),
+            nn.ReLU()
+        )
+        self.layer6 = nn.Sequential(
+            nn.ConvTranspose1d(n_out, n_in, kernel_size=kernel_size, padding=padding)
+        )
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        return out
