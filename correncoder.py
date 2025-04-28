131c6485f7f18cb7aca360c542e9d73fc1248bfd
Elham Farang <elham.fr80@gmail.com>
2025-04-28 17:38:58 +0330

    convert data to array

--- correncoder.py
+++ correncoder.py
@@ -4,7 +4,10 @@ import os
 import torch
 import numpy as np
 import random
-
+from sklearn.utils import shuffle
+from sklearn.model_selection import KFold
+from model2 import Correncoder_model
+import torch.nn as nn
 
 # a function that gives a rough indication of breaths per minute error by examining the crossings of 0.5
 # this assumes that the respiratory reference is normalised between 0 and 1.
@@ -29,7 +32,9 @@ def breaths_per_min_zc(output_array_zc, input_array_zc):
     return mean_abs_error, mean_error
 
 
-ppg_csv_path = "/Users/eli/Downloads/PPG Data/csv"
+# ppg_csv_path = "/Users/eli/Downloads/PPG Data/csv"
+ppg_csv_path = "/Users/elham/Downloads/csv/csv"
+
 ppg_csv_files = [f for f in os.listdir(ppg_csv_path) if f.endswith('.csv') and not f.startswith('.DS_Store')]
 input_name = 'PPG'
 target_name = 'NASAL CANULA'
@@ -40,7 +45,7 @@ num_of_subjects = 0
 
 # define number of epochs and batch size
 num_epochs = 80
-batch_size = 30
+batch_size = 8
 
 #define number of kernels per layer
 n_in, n_out = 1, 8
@@ -75,15 +80,13 @@ np.random.seed(seed_val)
 learning_rate = 0.001
 
 
-kf = KFold(42)
-kf.get_n_splits(data_ppg)
-sub_num = 1
+
 
 
 ppg_list = []
 resp_list = []
 
-for ppg_file in tqdm(ppg_csv_files, leave=True) :
+for ppg_file in tqdm(ppg_csv_files[:10], leave=True) :
     data = pd.read_csv(os.path.join(ppg_csv_path, ppg_file), sep='\t', index_col='Time', skiprows=[1])
     if input_name in data.columns and target_name in data.columns:
         num_of_subjects += 1
@@ -102,3 +105,98 @@ data_resp = np.stack(resp_list, axis=0)  # shape: (num_subjects, signal_length)
 
 print(f"data_ppg shape: {data_ppg.shape}")
 print(f"data_co2 shape: {data_resp.shape}")
+
+
+kf = KFold(42)
+kf.get_n_splits(data_ppg)
+sub_num = 1
+
+
+for train_index, test_index in kf.split(data_ppg):
+    trainX, testX = data_ppg[train_index, :], data_ppg[test_index, :]
+    trainy, testy = data_resp[train_index, :], data_resp[test_index, :]
+
+    # print which subject is current test subject
+    print(sub_num)
+
+    # shuffle training data
+    trainX, trainy = shuffle(trainX, trainy)
+
+    # set a model path for saving the trained pytorch model weights
+    model_path = "model_sub" + str(sub_num) + ".pth"
+
+    # initialise new model
+    model = Correncoder_model()
+    # Loss and optimizer
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+    # ensure correct shape, can also transpose here instead of reshaping
+    L_in = trainX.shape[-1]
+    trainX = trainX.reshape((trainX.shape[0], 1, L_in))
+    testX = testX.reshape((testX.shape[0], 1, L_in))
+
+    total_step = trainX.shape[0]
+
+    # transformation of data into torch tensors
+    trainXT = torch.from_numpy(trainX.astype('float32'))
+    # trainXT = trainXT.transpose(1,2).float() #input is (N, Cin, Lin) = Ntimesteps, Nfeatures, 128
+    trainyT = torch.from_numpy(trainy.astype('float32'))
+    testXT = torch.from_numpy(testX.astype('float32'))
+
+    testyT = torch.from_numpy(testy.astype('float32'))
+    # used for input to the breaths per minute calculator
+    input_array = testyT.cpu().detach().numpy()
+
+    loss_list = []
+    acc_list = []
+    acc_list_test_epoch = []
+    test_error = []
+
+    #begin training loop
+    for epoch in range(num_epochs):
+        for i in range(total_step // batch_size):  # split data into batches
+            trainXT_seg = trainXT[i * batch_size:(i + 1) * batch_size, :, :]
+            trainyT_seg = trainyT[i * batch_size:(i + 1) * batch_size, None]
+            # Run the forward pass
+            outputs = model(trainXT_seg)
+
+            loss = criterion(outputs, trainyT_seg)
+
+            #loss = d_loss_output[0]
+
+            loss_list.append(loss.item())
+
+            # Backprop and perform Adam optimisation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            
+        # calculate test error at the end of each epoch
+        test_output = model(testXT)
+        loss_test = criterion(test_output, testyT[:, None])
+        test_error.append((loss_test.item()))
+
+        output_array = test_output.cpu().detach().numpy()
+
+        mean_error_bpm = breaths_per_min_zc(output_array, input_array)
+
+        print("Test sub")
+        print(sub_num)
+        print("Epoch")
+        print(epoch)
+        print("Training loss")
+        print(loss)
+        print("Test loss")
+        print(loss_test)
+        print("Peaks error abs")
+        print(mean_error_bpm[0])
+        print("Peaks error bias")
+        print(mean_error_bpm[1])
+
+    # save the PyTorch model files
+    torch.save(model.state_dict(), model_path)
+    sub_num = sub_num + 1
+
